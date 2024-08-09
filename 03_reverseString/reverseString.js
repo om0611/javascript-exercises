0a5e115f3e371d2bc5910f98ae8c7781544823e4
Om Patel <[97650590+om0611@users.noreply.github.com]>
2024-08-09 16:39:05 -0400

    Finish javascript exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // let reversed = '';
+    // for (let i = string.length - 1; i >= 0; i--) {
+    //     reversed += string[i];
+    // }
+    // return reversed;
 
+    let array = string.split('');
+    array.reverse();
+    return array.join('');
 };
 
 // Do not edit below this line
