f7d4b80bf0b1eabbaad398f174eac438ac607aab
Om Patel <[97650590+om0611@users.noreply.github.com]>
2024-08-11 12:18:26 -0400

    Finish javascript exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  tempCelsius = (tempFahrenheit - 32) * (5 / 9);
+  return Number(tempCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  tempFahrenheit = tempCelsius * (9 / 5) + 32;
+  return Number(tempFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
