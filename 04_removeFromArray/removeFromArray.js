021588406af65d3c45f8f5d3ad9157980ac584de
Om Patel <[97650590+om0611@users.noreply.github.com]>
2024-08-11 11:36:42 -0400

    Finish javascript exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+    let new_array = array.filter(element => !(elementsToRemove.includes(element)));
+    return new_array;
 };
 
 // Do not edit below this line
