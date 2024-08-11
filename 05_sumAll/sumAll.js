836e9cd507ae2cad4a8ece24663a0949ff65566d
Om Patel <[97650590+om0611@users.noreply.github.com]>
2024-08-11 11:52:43 -0400

    Finish javascript exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof(num1) != 'number' || typeof(num2) != 'number' || 
+        num1 <= 0 || num2 <= 0 || 
+        Math.floor(num1) != num1 || Math.floor(num2) != num2) {
+        return 'ERROR';
+    }
+    let total = 0;
+    for (i=Math.min(num1, num2); i<=Math.max(num1, num2); i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
