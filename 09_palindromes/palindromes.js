1e918f33962d221044c831fe25be3fa9d95b67e0
Om Patel <[97650590+om0611@users.noreply.github.com]>
2024-11-19 12:03:54 -0500

    Completed exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,33 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.toLowerCase();
+  let l = 0;
+  let r = str.length - 1;
+  while (l <= r) {
+    while (!isAlphaNumeric(str, l)) {
+      l++;
+    }
+    while (!isAlphaNumeric(str, r)) {
+      r--;
+    }
+    if (str[l] != str[r]) {
+      return false;
+    }
+    l++;
+    r--;
+  }
+  return true;
 };
+
+function isAlphaNumeric(str, index) {
+  if (
+    !(str.charCodeAt(index) >= 48 && str.charCodeAt(index) <= 57) &&
+    !(str.charCodeAt(index) >= 97 && str.charCodeAt(index) <= 122)
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+}
 
 // Do not edit below this line
 module.exports = palindromes;
