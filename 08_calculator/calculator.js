d02512654775a23ed04c5f5079742b8f16a78652
Om Patel <[97650590+om0611@users.noreply.github.com]>
2024-11-19 11:37:37 -0500

    Completed exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sumSoFar, currNum) => sumSoFar + currNum, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((productSoFar, currNum) => productSoFar * currNum, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exp) {
+  return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let product = 1;
+  for (let i = 1; i <= num; i++) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
@@ -29,5 +33,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
