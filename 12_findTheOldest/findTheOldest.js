75485d9e4f883619542b0340f6bc98e42170301c
Om Patel <[97650590+om0611@users.noreply.github.com]>
2024-11-20 17:31:41 -0500

    Completed exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    people.sort(function (person1, person2) {
+        let person1_age;
+        let person2_age;
+        let currYear = new Date().getFullYear();
 
+        if (!("yearOfDeath" in person1)) {
+            person1_age = currYear - person1.yearOfBirth;
+        } else {
+            person1_age = person1.yearOfDeath - person1.yearOfBirth;
+        }
+        if (!("yearOfDeath" in person2)) {
+            person2_age = currYear - person2.yearOfBirth;
+        } else {
+            person2_age = person2.yearOfDeath - person2.yearOfBirth;
+        }
+        return person1_age - person2_age;
+    });
+    return people[people.length - 1];
 };
 
 // Do not edit below this line
