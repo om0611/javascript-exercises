78273e053b0a09f7a826937afbb08f99a6882fd0
Om Patel <[97650590+om0611@users.noreply.github.com]>
2024-11-20 17:31:41 -0500

    Completed exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  num = Number(num);
+  if (num < 0) {
+    return "OOPS";
+  } else if (num == 0) {
+    return 0;
+  } else if (num == 1) {
+    return 1;
+  }
+  let prev2 = 0;
+  let prev1 = 1;
+  for (let i = 2; i <= num; i++) {
+    [prev2, prev1] = [prev1, prev2 + prev1];
+  }
+  return prev1;
 };
 
 // Do not edit below this line
